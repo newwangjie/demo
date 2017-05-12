df3f24334ae43c3f9fec07f1293ff19763341e9b
WangJie <1033697770@qq.com>
2017-05-12 17:19:32 +0800

    first commit

--- js/focus.js
+++ js/focus.js
@@ -1,115 +1,136 @@
 (function($) {
-	$.fn.JieFocus = function(F) {
-		F = $.extend({
-			defaultIndex: 0,				// 默认显示第几个，第一个为 0
-			trigger: "click",				// 点击方式
-			showtime: 2000,					// 切换时间
-			showWay: "slow"					// 焦点图切换方式，slow 为渐隐渐现 down 为上下切换
-		}, F);
+  $.fn.JieFocus = function(F) {
+    F = $.extend({
+      defaultIndex: 0,        // 默认显示第几个，第一个为 0
+      trigger: "click",        // 点击方式
+      showtime: 2000,          // 切换时间
+      showWay: "slow"          // 焦点图切换方式，slow 为渐隐渐现 down 为上下切换
+    }, F);
 
-		// $(this)设置为全局变量
+    // $(this)设置为全局变量
+    var Obj = $(this);
+    function Status(){
+      Obj.find('img').each(function(){
+        // 浏览器的宽度
+        var BrowserWidth = parseInt($(window).width());
+        // 找到img的路径
+        var Src = $(this).attr('src');
+        // 给li添加背景图
+        $(this).parents('li').css("background-image","url(" + Src + ")");
+        //真实的宽度
+        var RealWidth = $(this).width();
+        console.log(RealWidth)
+        //  如果真实的宽度大于浏览器的宽度显示背景图片
+        if (RealWidth >= BrowserWidth) {
+          $(this).css("display","block");
+        } else {//如果小于浏览器的宽度按照原尺寸显示
+          $(this).css("display","none");
+        }
+      })
+    }
+    $(window).resize(function(){
+      Status();
+    });
 
-		var Obj = $(this);
+    $(function(){
+      Status();
+    });
 
-		// 添加class找到ul和li
-
-		Obj.children('ul').addClass('FocusPic');
+    // 添加class找到ul和li
+    Obj.children('ul').addClass('FocusPic');
     Obj.append('<ul class="FocusNum"></ul>')
-		var Ul = Obj.children('.FocusPic'),
-		 	Li = Ul.children('li'),
-			LiSize = Li.size();
+    var FocusPic = Obj.children('.FocusPic'),
+    // 找到li
+    FocusPicLi = FocusPic.children('li'),
+    // 找到li的个数
+    PicLiSize = FocusPicLi.size();
 
-		// 添加左右按钮div,获得li的个数
+    // 添加左右按钮div,获得li的个数
+    Obj.append('<div class="FocusLeft"></div><div class="FocusRight"></div>');
+    var Num = Obj.children('ul.FocusNum');
+    for (var i = 1; i <= PicLiSize; i++) {
+      Num.append('<li>'+i+'</li>');
+    }
 
-		Obj.append('<div class="FocusLeft"></div><div class="FocusRight"></div>');
-		var Num = Obj.children('ul.FocusNum');
-		for (var i = 1; i <= LiSize; i++) {
-			Num.append('<li>'+i+'</li>');
-		}
+    // 定义小点点，添加点击事件
 
-		// 定义小点点，添加点击事件
+    var NumLi = Num.children('li');
+    NumLi.each(function(){
+      var Current = $(this),
+        NunThis = Current.index();
+      if (NunThis == F.defaultIndex) {
+        Current.addClass('active');
+      }
+      Current.on(F.trigger,function() {
+        Current.addClass('active').siblings('li').removeClass('active');
+        PicSwitch(NunThis);
+      });
+    });
 
-		var NumLi = Num.children('li');
-		NumLi.each(function(){
-			var current = $(this),
-				I = current.index();
-			if (I==F.defaultIndex) {
-				current.addClass('active');
-			}
-			current.on(F.trigger,function() {
-				current.addClass('active').siblings('li').removeClass('active');
-				SandH(I);
-			});
-		});
+    // 设定向左向右按钮
 
-		// 设定向左向右按钮
+    var FocusLeft = Obj.children('.FocusLeft'),
+      FocusRight = Obj.children('.FocusRight');
 
-		var FL = Obj.children('.FocusLeft'),
-			FR = Obj.children('.FocusRight');
+    FocusLeft.click(function(){
+      GoLeft();
+    });
 
-		FL.click(function(){
-			GoLeft();
-		});
+    FocusRight.click(function(){
+      GoRight();
+    });
 
-		FR.click(function(){
-			GoRight();
-		});
+    function GoLeft(){
+      var NunThis = Num.children('li.active').index()-1;
+      if (NunThis == -1) {
+        var NunThis = PicLiSize - 1;
+      }
+      NumOn(NunThis);
+      PicSwitch(NunThis);
+    }
 
-		// 向左运动函数
+    function GoRight(){
+      var NunThis = Num.children('li.active').index()+1;
+      if (NunThis == PicLiSize) {
+        var NunThis = 0;
+      }
+      NumOn(NunThis);
+      PicSwitch(NunThis);
+    }
 
-		function GoLeft(){
-			var I = Num.children('li.active').index()-1;
-			if (I==-1) {
-				var I = LiSize-1;
-			}
-      NumOn(I);
-			SandH(I);
-		}
+    // 数字切换函数
 
-		// 向右运动函数
+    function NumOn(NunThis) {
+      NumLi.eq(NunThis).addClass("active").siblings().removeClass("active");
+    };
 
-		function GoRight(){
-			var I = Num.children('li.active').index()+1;
-			if (I==LiSize) {
-				var I = 0;
-			}
-			NumOn(I);
-			SandH(I);
-		}
+    // 焦点图图片切换
 
-		// 数字切换函数
+    function PicSwitch(NunThis) {
+      switch (F.showWay) {
+      case "down":
+        FocusPicLi.stop().eq(NunThis).fadeIn(400).siblings(this).fadeOut();
+        break;
+      default:
+        FocusPicLi.eq(NunThis).fadeIn(200).siblings().fadeOut();
+      }
+    };
 
-		function NumOn(I) {
-			NumLi.eq(I).addClass("active").siblings().removeClass("active");
-		};
+    // 定时器
 
-		// 图片切换函数
+    function ActionDo(){
+      return setInterval(function(){
+        GoRight();
+      },F.showtime);
+    };
 
-		function SandH(I) {
-			switch (F.showWay) {
-			case "down":
-        Li.stop().eq(I).fadeIn(400).siblings(this).fadeOut();
-				break;
-			default:
-				Li.eq(I).fadeIn(200).siblings().fadeOut();
-			}
-		};
+    // 当鼠标位于焦点图之上时清除定时器
 
-		// 定时器
-
-		function actionDo(){
-			return setInterval(function(){
-				GoRight();
-			},F.showtime);
-		};
-
-		// 当鼠标位于焦点图之上时清除定时器
-
-		var StopAct = actionDo();
-		Obj.hover(function() {
-			clearInterval(StopAct);
-		}, function() {
-			StopAct = actionDo();
-		});
-	}
+    var StopAct = ActionDo();
+    Obj.hover(function() {
+      clearInterval(StopAct);
+    }, function() {
+      StopAct = ActionDo();
+    });
+  }
 })(jQuery);
